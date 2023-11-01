62d15ad32ea4704bc81fda7fa980b2ba2ce82e11
root <mgiraudo@ies21.edu.ar>
2023-11-01 11:44:23 -0300

    Agregar archivos de proyecto.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp1
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Program program = new Program();
+			int resultado = program.Buscar();
+			Console.WriteLine("Resultado: " + resultado);
+		}
+		public int Buscar()
+		{
+			var target = 5;
+			var vec = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+			var i = 0;
+			while (i <= vec.Count)
+			{
+				if (vec[i] == target)
+				{
+					return i;
+				}
+				i++;
+			}
+			return -1;
+		}
+
+	}
+}
